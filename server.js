7cf38e06d2b54aa8a7dc8e94bba4c73b903f7fcf
Simon Goller <neosam@gmail.com>
2012-06-20 22:57:30 +0200

    Added node-server

---
+++ server.js
@@ -0,0 +1,33 @@
+/*  This file is part of Piratomat.
+
+    Piratomat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Piratomat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Piratomat.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*global require*/
+/*global __dirname*/
+(function() {
+    var staticServe = require('node-static'),
+        fileServer = new staticServe.Server(__dirname),
+        handler = function(request, response) {
+            request.addListener('end', function () {
+                fileServer.serve(request, response);
+            });
+        },
+        http = require('http'),
+        app = http.createServer(handler);
+        
+ 
+    app.listen(8080);
+
+})();
