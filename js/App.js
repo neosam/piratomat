f07b3dbd00d77d0e5b1ddc3bb63817ba5fc3299a
Simon Goller <neosam@gmail.com>
2012-06-22 23:04:56 +0200

    Questions are stored in questoins.js as json object

--- js/App.js
+++ js/App.js
@@ -1,51 +1,45 @@
 /*global define*/
 define([
         'jquery',
-        'underscore',
+        'use!underscore',
         'piratomat'
 ], function($, _, Piratomat){
-    var generateVotes = function() {
-        var vote1, vote2, vote3, all;
-        vote1 = new Piratomat.Vote({
-                question: 'Bla bla 1',
-                order: 0
+    var generateVotes = function(callback) {
+        var all = new Piratomat.VoteCollection();
+        $.getJSON('js/questions.js', function(data) {
+            _.each(data.questions, function(question) {
+                var vote = new Piratomat.Vote({
+                    question: question.question, /* wonderful line */
+                    order: 0
+                });
+                all.add(vote);
+            });
+            callback(all);
         });
-        vote2 = new Piratomat.Vote({
-                question: "Bla bla blub 2",
-                order: 1
-        });
-        vote3 = new Piratomat.Vote({
-                question: "asd;lfjasdfja 3",
-                order: 2
-        });
-        all = new Piratomat.VoteCollection();
-        all.add(vote1);
-        all.add(vote2);
-        all.add(vote3);
-        return all;
     };
 
     return function() {
-        var voteCollection = generateVotes(),
-            $pirateLogo = $('<img src="media/piraten-landshut.svg">'),
-            voteView = new Piratomat.VoteView({
-                model: voteCollection
-            }),
-            resultView = new Piratomat.ResultView({
-                model: voteCollection
-            });
-        $pirateLogo.addClass('logo');
-        $pirateLogo.appendTo('body');
-        voteView.showResultView = function() {
-            voteView.$el.hide();
-            resultView.$el.show();
-        };
-        resultView.doNewVote = function() {
-            resultView.$el.hide();
-            voteView.$el.show();
-        };
-        resultView.render().$el.hide();
-        voteView.render().$el.appendTo('body');
-        resultView.$el.appendTo('body');
+        generateVotes(function(voteCollection) {
+            var $pirateLogo = $('<img src="media/piraten-landshut.svg">'),
+                voteView = new Piratomat.VoteView({
+                    model: voteCollection
+                }),
+                resultView = new Piratomat.ResultView({
+                    model: voteCollection
+                });
+            $pirateLogo.addClass('logo');
+            $pirateLogo.appendTo('body');
+            voteView.showResultView = function() {
+                voteView.$el.hide();
+                resultView.$el.show();
+            };
+            resultView.doNewVote = function() {
+                resultView.$el.hide();
+                voteView.$el.show();
+            };
+            resultView.render().$el.hide();
+            voteView.render().$el.appendTo('body');
+            resultView.$el.appendTo('body');
+        });
     };
 });
---
+++ js/questions.js
@@ -0,0 +1,23 @@
+{
+	"id": "landshut1",
+	"questions": [
+		{
+			"question": "Glauben Sie, daß in den Parlamenten ihre Meinung vertreten ist?"
+		},
+		{
+			"question": "Sollen die Bürger bei wichtigen politischen Entscheidungen direkt befragt werden?"
+		},
+		{
+			"question": "Wenn Sie etwas verändern könnten, würden Sie sich politisch engagieren?"
+		},
+		{
+			"question": "Aus Ihrem bisherigen Informationsstand aus den Medien: Fühlen Sie sich gut darüber informiert, was die Piraten wollen?"
+		},
+		{
+			"question": "Wenn Sie \"Piratenpartei\" hören oder lesen, können Sie Positives damit in Verbindung bringen?"
+		},
+		{
+			"question": "Sind Ihnen einige Punkte aus dem Parteiprogramm der Piraten bekannt?"
+		}
+	]
+}
